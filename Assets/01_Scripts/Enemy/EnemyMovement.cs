b3b45d0ffb8c5cf0fcc0bdfbb31ab6a2e3f4cd57
minchae123 <08minchae17@gmail.com>
2023-11-02 15:41:33 +0900

    에셋 추가

---
+++ Assets/01_Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+	[SerializeField] private float speed = 1f;
+	[SerializeField] private GameObject player;
+	[SerializeField] private float range = 2f;
+
+	private Rigidbody2D rb;
+    private Vector2 movement;    
+
+	private void Awake()
+	{
+		player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    private void Update()
+    {
+        float dis = Vector3.Distance(transform.position, player.transform.position);
+        if(dis > range)
+		{
+            Vector3 direction = player.transform.position - transform.position;
+            direction.Normalize();
+            movement = direction;
+		}
+        else
+            movement = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter(movement);
+    }
+
+    private void MoveCharacter(Vector2 direction)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, transform.position, speed * Time.deltaTime);
+    }	
+}
