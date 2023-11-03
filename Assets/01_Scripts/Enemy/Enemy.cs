73c673070a5f4221075a847bca952bd7de332019
minchae123 <08minchae17@gmail.com>
2023-11-03 11:55:04 +0900

    음음음음음음 고민고민고민

--- Assets/01_Scripts/Enemy/Enemy.cs
+++ Assets/01_Scripts/Enemy/Enemy.cs
@@ -6,9 +6,16 @@ using UnityEngine.UI;
 public class Enemy : PoolableMono
 {
 	public List<LineType> enemyTypes;
-	public List<Image> show;
 	public int typeCount;
 	private int count;
+
+	public Dictionary<LineType, Sprite> showType = new Dictionary<LineType, Sprite>();
+	public List<Sprite> sprites;
+	private List<GameObject> typeList;
+	public GameObject imageParent;
+	public GameObject img;
+	private Sprite sprite;
+	
 
 	public override void Init()
 	{
@@ -17,11 +24,25 @@ public class Enemy : PoolableMono
 		{
 			int r = Random.Range(1, (int)LineType.END);
 			enemyTypes.Add((LineType)r);
+
+			if (showType.TryGetValue((LineType)r, out sprite))
+			{
+				sprites.Add(sprite);
+			}
+
+			GameObject g = Instantiate(img, imageParent.transform);
+			typeList.Add(g);
 		}
 	}
+	
 
 	private void Awake()
 	{
+		for (int i = 0; i < sprites.Count; i++)
+		{
+			showType.Add((LineType)i + 1, sprites[i]);
+		}
+
 		Init();
 	}
 
@@ -38,6 +59,8 @@ public class Enemy : PoolableMono
 		if (enemyTypes[0] == attack)
 		{
 			enemyTypes.RemoveAt(0);
+			Destroy(typeList[0]);
+			typeList.RemoveAt(0);
 		}
 	}
 
