0419a9e504dbd6482f27e355c95910b4a4a8114e
minchae123 <08minchae17@gmail.com>
2023-11-08 18:39:22 +0900

    ㅁㅇ

--- Assets/01_Scripts/Level.cs
+++ Assets/01_Scripts/Level.cs
@@ -7,30 +7,12 @@ using UnityEngine.UI;
 
 public class Level : MonoBehaviour
 {
-    [SerializeField] private ExperienceBar expBar;
-    [SerializeField] private TextMeshProUGUI levelText;
-    [SerializeField] private GameObject skillCanvas;
-
     int level = 1;
     int experience = 0;
 
     int TO_LEVEL_UP
     {
         get { return level * 20; }
-    }
-
-    private void Start()
-    {
-        expBar.UpdateExpBar(experience, TO_LEVEL_UP);
-        levelText.text = $"Level : {level}";
-    }
-
-    public void AddExperience(int amount)
-    {
-        experience += amount;
-        CheckLevelUp();
-        expBar.UpdateExpBar(experience, TO_LEVEL_UP);
-        levelText.text = $"Level : {level}";
     }
 
     public void CheckLevelUp()
