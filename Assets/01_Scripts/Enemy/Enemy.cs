fbbba2ec86506f4da28e12055dde241fe18c92b0
Inheoseongbin <seongbin529@gmail.com>
2023-11-09 19:34:48 +0900

    버그 고침 ㄹㅇ

--- Assets/01_Scripts/Enemy/Enemy.cs
+++ Assets/01_Scripts/Enemy/Enemy.cs
@@ -7,114 +7,120 @@ using System.Linq;
 
 public class Enemy : PoolableMono
 {
-	[Header("적 설정")]
-	public List<LineType> enemyTypes;
-	public int typeCount;
-	private int count;
-	public GameObject exp;
+    [Header("적 설정")]
+    public List<LineType> enemyTypes;
+    public int typeCount;
+    private int count;
+    public GameObject exp;
 
-	[Header("UI 설정")]
-	public List<Sprite> sprites = new List<Sprite>();
-	public GameObject imageParent;
-	public GameObject img;
+    [Header("UI 설정")]
+    public List<Sprite> sprites = new List<Sprite>();
+    public GameObject imageParent;
+    public GameObject img;
 
-	private Dictionary<LineType, Sprite> showType = new Dictionary<LineType, Sprite>();
-	private List<GameObject> typeList = new List<GameObject>();
-	private Sprite sprite;
+    private Dictionary<LineType, Sprite> showType = new Dictionary<LineType, Sprite>();
+    private List<GameObject> typeList = new List<GameObject>();
+    private Sprite sprite;
 
-	private SpriteRenderer _sr;
-	private readonly int _dissolve = Shader.PropertyToID("_Dissolve");
-	private readonly string _isDissolve = "_IsDissolve";
-	private readonly string _isHit = "_IsSolidColor";
+    private SpriteRenderer _sr;
+    [SerializeField] private SpriteRenderer _emo;
+    private readonly int _dissolve = Shader.PropertyToID("_Dissolve");
+    private readonly string _isDissolve = "_IsDissolve";
+    private readonly string _isHit = "_IsSolidColor";
 
-    bool _isDead = false;
-	private BoxCollider2D _hitDecision;
+    public bool _isDead = false;
+    private BoxCollider2D _hitDecision;
 
-	public override void Init()
-	{
+    public override void Init()
+    {
+        _emo.enabled = true;
         _sr.material.SetInt(_isHit, 0);
         _sr.material.SetInt(_isDissolve, 0);
         _hitDecision.enabled = true;
-		_isDead = false;
+        _isDead = false;
 
-		//쉐이더 값 초기화
-		_sr.material.SetFloat(_dissolve, 1f);
+        //쉐이더 값 초기화
+        _sr.material.SetFloat(_dissolve, 1f);
 
-		count = typeCount;
-		for (int i = 0; i < count; i++) // 이제 개수만큼 랜덤으로 애마다 공격 타입 받아주기
-		{
-			int r = Random.Range(1, (int)LineType.END);
-			enemyTypes.Add((LineType)r);
+        count = typeCount;
+        for (int i = 0; i < count; i++) // 이제 개수만큼 랜덤으로 애마다 공격 타입 받아주기
+        {
+            int r = Random.Range(1, (int)LineType.END);
+            enemyTypes.Add((LineType)r);
 
-			if (showType.TryGetValue((LineType)r, out sprite))
-			{
-				sprites.Add(sprite);
-				GameObject g = Instantiate(img, imageParent.transform);
-				g.GetComponent<Image>().sprite = sprite;
-				typeList.Add(g);
-			}
-		}
-	}
-	
-	private void Awake()
-	{
+            if (showType.TryGetValue((LineType)r, out sprite))
+            {
+                sprites.Add(sprite);
+                GameObject g = Instantiate(img, imageParent.transform);
+                g.GetComponent<Image>().sprite = sprite;
+                typeList.Add(g);
+            }
+        }
+    }
+
+    private void Awake()
+    {
         _sr = GetComponentInChildren<SpriteRenderer>();
         _hitDecision = GetComponent<BoxCollider2D>();
 
-		for (int i = 0; i < sprites.Count; i++) // 처음에 딕셔너리에 타입이랑 그림 넣어
-		{
-			showType.Add((LineType)i + 1, sprites[i]);
-		}
-	}
+        for (int i = 0; i < sprites.Count; i++) // 처음에 딕셔너리에 타입이랑 그림 넣어
+        {
+            showType.Add((LineType)i + 1, sprites[i]);
+        }
+    }
 
-	private void Update()
-	{
-		if(enemyTypes.Count == 0 && !_isDead) // 남은 타입이 없으면 다 없앴으니까 죽일거임
-		{
-			Die();
-		}
-	}
+    private void Update()
+    {
+        if (enemyTypes.Count == 0 && !_isDead) // 남은 타입이 없으면 다 없앴으니까 죽일거임
+        {
+            Die();
+        }
+    }
 
-	public void PlayerDraw(LineType attack)
-	{
-		if (enemyTypes[0] == attack && !_isDead) // 딕셔너리 타입이랑 첫번째꺼의 타입이 같으면 하나 지울거얌
-		{
-			DrawReduce(0);
-		}
-	}
+    public void PlayerDraw(LineType attack)
+    {
+        if (enemyTypes.Count > 0 || !_isDead)
+        {
+            if (enemyTypes[0] == attack) // 딕셔너리 타입이랑 첫번째꺼의 타입이 같으면 하나 지울거얌
+            {
+                DrawReduce(0);
+            }
+        }
+    }
 
-	public void DrawReduce(int id)
-	{
-		if(enemyTypes.Any()) // 여러개의 공격을 한번에 받아서 지울게 하나밖에 없을때 
-		{
-			enemyTypes.RemoveAt(id);
-			Destroy(typeList[id]);
-			typeList.RemoveAt(id);
-			StartCoroutine(Hit());
-		}
-		else
-			return;
-	}
+    public void DrawReduce(int id)
+    {
+        if (enemyTypes.Any()) // 여러개의 공격을 한번에 받아서 지울게 하나밖에 없을때 
+        {
+            enemyTypes.RemoveAt(id);
+            Destroy(typeList[id]);
+            typeList.RemoveAt(id);
+            StartCoroutine(Hit());
+        }
+        else
+            return;
+    }
 
     private IEnumerator Hit()
-	{
+    {
         _sr.material.SetInt(_isHit, 1);
         yield return new WaitForSeconds(.1f);
         _sr.material.SetInt(_isHit, 0);
-	}
+    }
 
 
     public void Die()
-	{
-		_isDead = true;
+    {
+        _isDead = true;
         _hitDecision.enabled = false;
-		StartCoroutine(DieDissolve(1));
-		FallExp();
-	}
+        _emo.enabled = false;
+        StartCoroutine(DieDissolve(1));
+        FallExp();
+    }
 
     private IEnumerator DieDissolve(float time)
     {
-		_sr.material.SetInt(_isDissolve, 1);
+        _sr.material.SetInt(_isDissolve, 1);
         float currentRate;
         float percent = 0;
         float currentTime = 0;
@@ -133,12 +139,12 @@ public class Enemy : PoolableMono
     }
 
     private void FallExp() // Exp 떨구기
-	{
-		Experience r = PoolManager.Instance.Pop(exp.name) as Experience;
-		r.transform.position = transform.position;
+    {
+        Experience r = PoolManager.Instance.Pop(exp.name) as Experience;
+        r.transform.position = transform.position;
 
-		Vector3 offset = Random.insideUnitCircle;
+        Vector3 offset = Random.insideUnitCircle;
 
-		r.transform.DOJump(transform.position + offset, 2, 1, 0.4f);
-	}
+        r.transform.DOJump(transform.position + offset, 2, 1, 0.4f);
+    }
 }
