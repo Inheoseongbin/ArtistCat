cf14e26895ac81283bbfff456d9b896fbb8b532c
Inheoseongbin <seongbin529@gmail.com>
2023-11-13 18:06:11 +0900

    고침

--- Assets/01_Scripts/Skill/Heal.cs
+++ Assets/01_Scripts/Skill/Heal.cs
@@ -6,6 +6,11 @@ public class Heal : PoolableMono
 {
     public override void Init()
     {
+        Invoke("HealParticle", 1);
+    }
+
+    void HealParticle()
+    {
         PoolManager.Instance.Push(this);
     }
 }
