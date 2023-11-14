5fd468f99ef9d1959b67991ad09524c1a6d00fc6
dksgudwn <dksgudwn620@gmail.com>
2023-11-14 19:47:24 +0900

    부메랑 이것저것 완료

--- Assets/01_Scripts/Skill/Boomerang.cs
+++ Assets/01_Scripts/Skill/Boomerang.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,18 +10,50 @@ public class Boomerang : MonoBehaviour
 
     List<Rotater> _rotaterList = new List<Rotater>();
 
+    bool isVisible = true;
+
+    private void Start()
+    {
+        StartCoroutine(Blink());
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && level < 6)//6개로 제한하는게 좋은것 같습니다.
         {
             LevelUp();
         }
+        if (isVisible)
+        {
+            for (int i = 0; i < _rotaterList.Count; i++)
+            {
+                _rotaterList[i].gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < _rotaterList.Count; i++)
+            {
+                _rotaterList[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            isVisible = false;
+            yield return new WaitForSeconds(2f);
+            isVisible = true;
+            yield return new WaitForSeconds(4f);
+        }
     }
 
     public void LevelUp()
     {
         level++;
-
+        isVisible = true;
         Rotater r = Instantiate(_rotater, transform);
         //r.transform.parent = transform;
         _rotaterList.Add(r);
