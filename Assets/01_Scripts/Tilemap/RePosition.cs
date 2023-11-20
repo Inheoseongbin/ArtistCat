8703f9022733732a05f657e9bf20b19cdd94630f
dksgudwn <dksgudwn620@gmail.com>
2023-11-20 18:39:17 +0900

    진짜진짜 완료

--- Assets/01_Scripts/Tilemap/RePosition.cs
+++ Assets/01_Scripts/Tilemap/RePosition.cs
@@ -14,15 +14,6 @@ public class RePosition : MonoBehaviour
         coll = GetComponent<Collider2D>();
     }
 
-    private void Update()
-    {
-        //Debug.Log(Vector2.Distance(GameManager.Instance.playerTrm.position, transform.position));
-        //if (Vector2.Distance(GameManager.Instance.playerTrm.position, transform.position) > 50)
-        //{
-        //    Debug.LogError("Reset");
-        //    ResetPosition();
-        //}
-    }
 
     //맵이랑 에리아랑 충돌을 마치면 
     //이 스크립트는 맵임
@@ -31,21 +22,17 @@ public class RePosition : MonoBehaviour
         if (!collision.CompareTag("Area"))
             return;
 
-        //Player의 거리 확인. 어디에 재배치를 할까?
+        // 플레이어 위치에 따른 움직임 방향 계산
         Vector3 playerPos = GameManager.Instance.playerTrm.position;
         Vector3 myPos = transform.position;
         float diffX = Mathf.Abs(playerPos.x - myPos.x);
         float diffY = Mathf.Abs(playerPos.y - myPos.y);
 
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        Vector3 movement = new Vector3(h, v, 0);
-        float dirX = movement.x < 0 ? -1 : 1;
-        float dirY = movement.y < 0 ? -1 : 1;
+        float dirX = playerPos.x < myPos.x ? -1 : 1;
+        float dirY = playerPos.y < myPos.y ? -1 : 1;
 
-        if (transform.tag == "Ground")
+        if (transform.CompareTag("Ground"))
         {
-            //Debug.LogError()
             if (diffX >= diffY)
             {
                 transform.Translate(Vector3.right * dirX * 40);
@@ -55,27 +42,7 @@ public class RePosition : MonoBehaviour
                 transform.Translate(Vector3.up * dirY * 40);
             }
             //Debug.Log($"{transform.gameObject.name} : {Vector2.Distance(playerPos, transform.position)}");
-
         }
     }
 
-    private void ResetPosition()
-    {
-        Debug.LogError("Reset");
-        switch (number)
-        {
-            case 0:
-                transform.position = new Vector2(GameManager.Instance.playerTrm.position.x, GameManager.Instance.playerTrm.position.y);
-                break;
-            case 1:
-                transform.position = new Vector2(GameManager.Instance.playerTrm.position.x, GameManager.Instance.playerTrm.position.y);
-                break;
-            case 2:
-                transform.position = new Vector2(GameManager.Instance.playerTrm.position.x, GameManager.Instance.playerTrm.position.y);
-                break;
-            case 3:
-                transform.position = new Vector2(GameManager.Instance.playerTrm.position.x, GameManager.Instance.playerTrm.position.y);
-                break;
-        }
-    }
 }
