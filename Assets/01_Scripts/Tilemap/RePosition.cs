f12d682952186d8541764f437db64008f71f837e
dksgudwn <dksgudwn620@gmail.com>
2023-11-20 18:22:29 +0900

    123

--- Assets/01_Scripts/Tilemap/RePosition.cs
+++ Assets/01_Scripts/Tilemap/RePosition.cs
@@ -1,16 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class RePosition : MonoBehaviour
 {
     Collider2D coll;
-
+    public int number;
     private void Awake()
     {
         coll = GetComponent<Collider2D>();
     }
 
+    private void Update()
+    {
+        //Debug.Log(Vector2.Distance(GameManager.Instance.playerTrm.position, transform.position));
+        //if (Vector2.Distance(GameManager.Instance.playerTrm.position, transform.position) > 50)
+        //{
+        //    Debug.LogError("Reset");
+        //    ResetPosition();
+        //}
+    }
 
     //맵이랑 에리아랑 충돌을 마치면 
     //이 스크립트는 맵임
@@ -20,7 +32,7 @@ public class RePosition : MonoBehaviour
             return;
 
         //Player의 거리 확인. 어디에 재배치를 할까?
-        Vector3 playerPos = GameManager.Instance.playerTrm.transform.position;
+        Vector3 playerPos = GameManager.Instance.playerTrm.position;
         Vector3 myPos = transform.position;
         float diffX = Mathf.Abs(playerPos.x - myPos.x);
         float diffY = Mathf.Abs(playerPos.y - myPos.y);
@@ -43,7 +55,27 @@ public class RePosition : MonoBehaviour
                 transform.Translate(Vector3.up * dirY * 40);
             }
             //Debug.Log($"{transform.gameObject.name} : {Vector2.Distance(playerPos, transform.position)}");
-            
+
+        }
+    }
+
+    private void ResetPosition()
+    {
+        Debug.LogError("Reset");
+        switch (number)
+        {
+            case 0:
+                transform.position = new Vector2(GameManager.Instance.playerTrm.position.x, GameManager.Instance.playerTrm.position.y);
+                break;
+            case 1:
+                transform.position = new Vector2(GameManager.Instance.playerTrm.position.x, GameManager.Instance.playerTrm.position.y);
+                break;
+            case 2:
+                transform.position = new Vector2(GameManager.Instance.playerTrm.position.x, GameManager.Instance.playerTrm.position.y);
+                break;
+            case 3:
+                transform.position = new Vector2(GameManager.Instance.playerTrm.position.x, GameManager.Instance.playerTrm.position.y);
+                break;
         }
     }
 }
