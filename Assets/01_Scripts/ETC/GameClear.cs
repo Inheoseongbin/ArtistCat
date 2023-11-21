78b6f99820cecaebfed44e7fc5c724291a1176fc
minchae123 <08minchae17@gmail.com>
2023-11-21 13:04:02 +0900

    스토리

--- Assets/01_Scripts/ETC/GameClear.cs
+++ Assets/01_Scripts/ETC/GameClear.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class GameClear : MonoBehaviour
 {
 	[SerializeField] private List<GameObject> stories;
+	[SerializeField] private List<TextMeshProUGUI> storyText;
 	[SerializeField] private GameObject story;
 
 	private void Start()
@@ -21,12 +23,18 @@ public class GameClear : MonoBehaviour
 
 	IEnumerator StoryStart()
 	{
+		storyText[0].maxVisibleCharacters = 0;
+		storyText[1].maxVisibleCharacters = 0;
 		yield return new WaitForSeconds(0.2f);
 		stories[0].transform.DOLocalMoveY(94, 1);
 		yield return new WaitForSeconds(1);
-		stories[1].transform.DOLocalMoveY(-96, 1);
-
+		TMPTextTyping(2f, storyText[0]);
 		yield return new WaitForSeconds(2);
+		stories[1].transform.DOLocalMoveY(-96, 1);
+		yield return new WaitForSeconds(1);
+		TMPTextTyping(2, storyText[1]);
+
+		yield return new WaitForSeconds(4);
 		story.transform.DOLocalMoveY(-1100, 1);
 	}
 
@@ -39,5 +47,11 @@ public class GameClear : MonoBehaviour
 			pos.y = -960;
 			g.transform.localPosition = pos;
 		}
+	}
+
+	private void TMPTextTyping(float time, TextMeshProUGUI txt)
+	{
+		txt.maxVisibleCharacters = 0;
+		DOTween.To(x => txt.maxVisibleCharacters = (int)x, 0f, txt.text.Length, time);
 	}
 }
--- Assets/01_Scripts/ETC/Story.cs
+++ Assets/01_Scripts/ETC/Story.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class Story : MonoBehaviour
 {
 	[SerializeField] private List<GameObject> openingStories;
+	[SerializeField] private List<TextMeshProUGUI> storyText;
 
 	#region ÀÎÆ®·Î
 	public void IntroStory()
@@ -16,12 +18,21 @@ public class Story : MonoBehaviour
 
 	IEnumerator IStoryStart()
 	{
+		storyText[0].maxVisibleCharacters = 0;
+		storyText[1].maxVisibleCharacters = 0;
+		storyText[2].maxVisibleCharacters = 0;
 		yield return new WaitForSeconds(0.2f);
 		openingStories[0].transform.DOLocalMoveY(190, 1);
 		yield return new WaitForSeconds(1);
+		TMPTextTyping(1.3f, storyText[0]);
+		yield return new WaitForSeconds(1.3f);
 		openingStories[1].transform.DOLocalMoveY(-205, 1);
 		yield return new WaitForSeconds(1);
+		TMPTextTyping(1.3f, storyText[1]);
+		yield return new WaitForSeconds(1.3f);
 		openingStories[2].transform.DOLocalMoveY(203, 1);
+		yield return new WaitForSeconds(1);
+		TMPTextTyping(1.3f, storyText[2]);
 	}
 
 	public void IResetStory()
@@ -33,6 +44,13 @@ public class Story : MonoBehaviour
 			pos.y = -960;
 			g.transform.localPosition = pos;
 		}
+	}
+
+
+	private void TMPTextTyping(float time, TextMeshProUGUI txt)
+	{
+		txt.maxVisibleCharacters = 0;
+		DOTween.To(x => txt.maxVisibleCharacters = (int)x, 0f, txt.text.Length, time);
 	}
 	#endregion
 }
